d44a94b948ad2291a14487f9a29e4967978255a8
abdaalsy <abdaalsy@gmail.com>
2025-07-16 23:05:35 -0400

    switch from array to Vec2 struct

--- main.cpp
+++ main.cpp
@@ -10,9 +10,9 @@ using json = nlohmann::json;
 int main() {
     std::cout << "zero vector field test:" << std::endl;
     Scene* scene = new Scene({0.0, 0.0}, {11, 10}, 1.0, 0.1);
-    std::cout << "Bottom left: (" << scene->bottomLeft[0] << ", " << scene->bottomLeft[1] << ")" << std::endl;
-    std::cout << "top right: (" << scene->topRight[0] << ", " << scene->topRight[1] << ")" << std::endl;
-    std::vector<std::vector<std::array<double, 2>>> field = scene->zeroVectorField(scene->bottomLeft, scene->topRight, scene->deltaX);
+    std::cout << "Bottom left: (" << scene->bottomLeft.x << ", " << scene->bottomLeft.y << ")" << std::endl;
+    std::cout << "top right: (" << scene->topRight.x << ", " << scene->topRight.y << ")" << std::endl;
+    std::vector<std::vector<Vec2>> field = scene->zeroVectorField(scene->bottomLeft, scene->topRight, scene->deltaX);
     printVectorField(field);
 
     std::cout << scene->timesteps << std::endl;
