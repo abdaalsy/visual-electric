f9d5f0ad1c74e59c77431add831245e8544bbc24
abdaalsy <abdaalsy@gmail.com>
2025-07-17 01:58:59 -0400

    test flattening and unflattening

--- main.cpp
+++ main.cpp
@@ -1,21 +1,38 @@
 #include <iostream>
 #include <string>
 #include <array>
-#include "include/parse.hpp"
-#include "include/velectric.hpp"
-#include "libs/Eigen/Dense"
+#include <parse.hpp>
+#include <velectric.hpp>
+#include <Eigen/Dense>
 
 using json = nlohmann::json;
 
 int main() {
     std::cout << "zero vector field test:" << std::endl;
-    Scene* scene = new Scene({0.0, 0.0}, {11, 10}, 1.0, 0.1);
+    Vec2 bottomLeft = {0.0, 0.0};
+    Vec2 topRight = {5, 5};
+    double dx = 1.0;
+    double dt = 0.1;
+    Scene* scene = new Scene(bottomLeft, topRight, dx, dt);
     std::cout << "Bottom left: (" << scene->bottomLeft.x << ", " << scene->bottomLeft.y << ")" << std::endl;
     std::cout << "top right: (" << scene->topRight.x << ", " << scene->topRight.y << ")" << std::endl;
-    std::vector<std::vector<Vec2>> field = scene->zeroVectorField(scene->bottomLeft, scene->topRight, scene->deltaX);
+    std::vector<std::vector<Vec2>> field = scene->zeroVectorField(scene->bottomLeft, scene->topRight, scene->dx);
     printVectorField(field);
 
     std::cout << scene->timesteps << std::endl;
+
+    std::cout << std::endl << "vector field flattening test:" << std::endl;
+    Vec2 flatField[5*5];
+    Scene::flatten(flatField, field);
+    for (auto &vec : flatField)
+    {
+        std::cout << "[" << vec.x << ", " << vec.y << "]  ";
+    }
+
+    std::cout << std::endl << "vector field unflattening test:" << std::endl;
+    std::vector<std::vector<Vec2>> unflatField(5, std::vector<Vec2>(5));
+    Scene::unflatten(unflatField, flatField, 25, 5);
+    printVectorField(unflatField);
 
     delete scene;
     scene = nullptr;
